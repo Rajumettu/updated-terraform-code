580e605880d8b254f767290b2aeee2e4f860c06f
Rajumettu <157581206+Rajumettu@users.noreply.github.com>
2024-02-05 20:32:47 +0530

    Create sg.tf

---
+++ sg.tf
@@ -0,0 +1,27 @@
+resource "aws_security_group" "raju-sg2" {
+  name        = "raju-sg2"
+  vpc_id      = "<>"
+
+  ingress {
+    description = "SSH Access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP Access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
